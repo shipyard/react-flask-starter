005f62d2a723012c1b5bb1c0502778a200cb1a43
elliott-king <8884571+elliott-king@users.noreply.github.com>
2021-12-21 12:00:53 -0500

    Add DOOM url env var

--- frontend/src/components/DoomModal.js
+++ frontend/src/components/DoomModal.js
@@ -1,8 +1,9 @@
 function DoomModal(props) {
   const {classes} = props;
+  const doomUrl = process.env.REACT_APP_DOOM_URL || "http://localhost:8000"
   return (
     <div className={classes.modal} id="doom-container">
-        <iframe className={classes.iframe} scrolling="no" src="http://localhost:8000" title="DOOM WASM"></iframe>
+        <iframe className={classes.iframe} scrolling="no" src={doomUrl} title="DOOM WASM"></iframe>
     </div>
   )
 }
